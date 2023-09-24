adc18913507df8c621565d69661353b0e1161a0d
1XSTRY <kosogornov99@gmail.com>
2023-09-24 13:09:47 +0500

    Complete task 2

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,18 @@ public class Main {
         char iOS = 0;
         char android = 1;
         int clientOs =1;
+        char oldModel = 2014;
+        char newModel = 2023;
+        int clientModel = 2014;
 
-        if (clientOs == 1){
-            System.out.println("Установите версию приложения для " + android +" по ссылке");
-        }else {
-            System.out.println("Установите версию приложения для " + iOS + " по ссылке");
+        if (clientOs == 1 && clientModel >= 2015){
+            System.out.println("Установите версию приложения для android по ссылке");
+        }else if (clientOs == 0 && clientModel >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }else if (clientOs == 1 && clientModel < 2015) {
+            System.out.println("Установите облегченную версию приложения для android по ссылке");
+        } else if (clientOs == 0 && clientModel < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         }
     }
 
