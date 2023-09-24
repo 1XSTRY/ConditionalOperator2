124079be78b13800f24569c6635bfcb32fb25d47
1XSTRY <kosogornov99@gmail.com>
2023-09-24 14:04:41 +0500

    Complete task 5

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         //task2();
         task3();
         task4();
-       //task5();
+        task5();
     }
     public static void task1 (){
         char iOS = 0;
@@ -52,6 +52,34 @@ public class Main {
             return 3;
         } else {
             return -1;
+        }
+    }
+    public static void task5 (){
+        int monthNumber = 12;
+
+        switch (monthNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.err.println("Номер месяца должен быть от 1 до 12.");
         }
     }
 
