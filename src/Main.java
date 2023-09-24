e048b235b50b0c458655be8b0f8174dab24d061c
1XSTRY <kosogornov99@gmail.com>
2023-09-24 12:50:02 +0500

    Complete task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-
+        task1();
+        //task2();
+        //task3();
+        //task4();
+       //task5();
     }
+    public static void task1 (){
+        char iOS = 0;
+        char android = 1;
+        int clientOs =1;
+
+        if (clientOs == 1){
+            System.out.println("Установите версию приложения для " + android +" по ссылке");
+        }else {
+            System.out.println("Установите версию приложения для " + iOS + " по ссылке");
+        }
+    }
+
+
 }
