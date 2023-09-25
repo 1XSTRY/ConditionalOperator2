e3d72b08f1dd1c7a769f5ad98f552de1eb8aa140
1XSTRY <kosogornov99@gmail.com>
2023-09-25 19:14:21 +0500

    Bug fix

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-        //task2();
+        task2();
         task3();
         task4();
         task5();
@@ -10,48 +10,66 @@ public class Main {
         char iOS = 0;
         char android = 1;
         int clientOs =1;
-        char oldModel = 2014;
-        char newModel = 2023;
-        int clientModel = 2014;
 
-        if (clientOs == 1 && clientModel >= 2015){
+        if (clientOs == android){
             System.out.println("Установите версию приложения для android по ссылке");
-        }else if (clientOs == 0 && clientModel >= 2015) {
+        }else if (clientOs == iOS) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        }else if (clientOs == 1 && clientModel < 2015) {
+        }
+    }
+    public static void task2 (){
+        char iOS = 0;
+        char android = 1;
+        int clientOs =1;
+        char oldModel = 2015;
+        char newModel = 2016;
+        int clientModel = 2013;
+
+        if (clientOs == android && clientModel >= newModel){
+            System.out.println("Установите версию приложения для android по ссылке");
+        }else if (clientOs == iOS && clientModel >= newModel) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }else if (clientOs == android && clientModel < oldModel) {
             System.out.println("Установите облегченную версию приложения для android по ссылке");
-        } else if (clientOs == 0 && clientModel < 2015) {
+        } else if (clientOs == iOS && clientModel < oldModel) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         }
     }
     public static void task3 (){
         int year = 2021;
 
-        if (year >= 1584 && year % 4 == 0) {
+        if (year >= 1584 && year % 400 == 0) {
             System.out.println(year + " является високосным годом");
         }
-            else {
+        else if (year >= 1584 && year % 4 == 0 && year % 100 != 0){
+            System.out.println(year + " является високосным годом");
+        }
+        else {
             System.out.println(year + " не является високосным годом");
         }
+
 
 
 
     }
     public static void task4 (){
         int deliveryDistance = 95;
-        int days = getDeliveryDays(deliveryDistance);
-        System.out.println("Потребуется дней: " + days);
-    }
+        int deliveryTime;
 
-    public static int getDeliveryDays(int deliveryDistance) {
-        if (deliveryDistance <= 20) {
-            return 1;
+        if (deliveryDistance >=1 && deliveryDistance <= 20) {
+            deliveryTime = 1;
         } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
-            return 2;
+            deliveryTime = 2;
         } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
-            return 3;
+            deliveryTime = 3;
         } else {
-            return -1;
+            deliveryTime = -1;
+        }
+
+        if (deliveryTime != -1) {
+            System.out.println("Потребуется дней: " + deliveryTime);
+        } else {
+            System.out.println("Доставка невозможна");
         }
     }
     public static void task5 (){
